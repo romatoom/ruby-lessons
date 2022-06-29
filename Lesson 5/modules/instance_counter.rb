5cc157ac016fb57456203f2eb4ed57b05f4d7d94
Roman Ivanov <romani@epayservices.com>
2022-06-29 22:00:36 +0500

    Lesson 5: fix increment_instances

--- Lesson 5/modules/instance_counter.rb
+++ Lesson 5/modules/instance_counter.rb
@@ -7,12 +7,12 @@ module InstanceCounter
   module ClassMethods
     attr_reader :instances
 
+    private
+
     def increment_instances
       self.instances ||= 0
       self.instances += 1
     end
-
-    private
 
     attr_writer :instances
   end
@@ -21,7 +21,7 @@ module InstanceCounter
     private
 
     def register_instance
-      self.class.increment_instances
+      self.class.send :increment_instances
     end
   end
 end
